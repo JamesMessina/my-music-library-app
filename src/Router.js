7dbf71543e26b35f8a7ebefa05940e108fd7f34f
James Messina <jmessina17@gmail.com>
2021-09-05 16:10:35 -0500

    finished login and logout functionality

--- src/Router.js
+++ src/Router.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router'
 import Home from './containers/Home'
 import Band from './containers/Band'
 import LoginPage from './containers/Login'
+import SignUpPage from './components/SignUpPage'
 
 
 const Router = () => {
     return (
         <Switch>
+            <Route exact path="/signup" component={SignUpPage} />
             <Route exact path="/" component={LoginPage} />
             <Route exact path="/home" component={Home} />
             <Route exact path="/band/:history_id" component={Band} />
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { AppBar, Toolbar, IconButton, 
-    Typography } from '@material-ui/core'
+    Typography, 
+    Button} from '@material-ui/core'
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
 import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 
 
 const Navigation = () => {
+
+    const history = useHistory(); 
+
+    function logout(){
+        localStorage.removeItem("token");
+        localStorage.removeItem("name"); 
+
+        history.push("/")
+    }
+
     return (
         <AppBar style={{fontFamily: "'Besley', serif", marginBottom: '10px'}} position="relative">
             <Toolbar>
@@ -17,14 +29,18 @@ const Navigation = () => {
                 </Typography>
                 <ul className="nav-list">
                     <li className="nav-list-item">
-                        <Link to="/">Login</Link>
+                        <Link style= {{fontFamily: "'Kaisei HarunoUmi', serif" }}to="/">Login</Link>
                     </li>
                     <li className="nav-list-item">
-                        <Link to="/home">Home</Link>
+                        <Link style= {{fontFamily: "'Kaisei HarunoUmi', serif" }} to="/home">Home</Link>
                     </li>
                     <li className="nav-list-item">
-                        <Link to="/import">Post Band</Link>
+                        <Link style= {{fontFamily: "'Kaisei HarunoUmi', serif" }} to="/import">Post Band</Link>
                     </li>
+                    <li>
+                        <Button style={{ backgroundColor: "transparent", fontSize: "16px", textTransform: 'none', fontFamily: "'Kaisei HarunoUmi', serif"}} color="inherit" onClick={logout}>Logout</Button>
+                    </li>
+                    
                 </ul>
             </Toolbar>
         </AppBar>
